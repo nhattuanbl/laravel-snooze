06e754d2063d5d11dd67193f265fb2f6eb63d321
nhattuanbl <user.email>
2023-12-20 20:40:21 +0700

    0.8.2

--- config/logging.php
+++ config/logging.php
@@ -1,13 +1,11 @@
 <?php
 
 return [
-    'channels' => [
-        'notify_snooze' => [
-            'driver' => 'daily',
-            'path' => storage_path('logs/notify_snooze.log'),
-            'level' => 'debug',
-            'days' => 30,
-            'replace_placeholders' => true,
-        ]
+    'notify_snooze' => [
+        'driver' => 'daily',
+        'path' => storage_path('logs/notify_snooze.log'),
+        'level' => 'debug',
+        'days' => 30,
+        'replace_placeholders' => true,
     ]
 ];
